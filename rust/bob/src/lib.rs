8f1852dc49fb4a5fcf8b1b85b1bababe150f3504
roger <roger.duran@mobilityhouse.com>
2019-05-28 22:15:45 +0200

    improve rust/bob readability

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,26 +1,35 @@
+// Responses
+const EMPTY: &str = "Fine. Be that way!";
+const SHOUTING: &str = "Whoa, chill out!";
+const QUESTION: &str = "Sure.";
+const RANDOM_MSG: &str = "Whatever.";
+const SHOUTING_QUESTION: &str = "Calm down, I know what I'm doing!";
+
+/// Check if all the alphabetic characters of a message are upercase
 fn is_shouting(message: &str) -> bool {
   let letters: String = message
       .chars()
       .filter(|c| c.is_alphabetic())
       .collect();
 
-  letters.len() != 0 && letters.chars().all(|c| c.is_uppercase())
+  !letters.is_empty() && letters.chars().all(|c| c.is_uppercase())
 }
 
 
 pub fn reply(message: &str) -> &str {
   let message = message.trim();
   let is_shouting = is_shouting(message);
+  let is_question = message.ends_with("?");
 
   if message.is_empty() {
-    "Fine. Be that way!"
-  } else if is_shouting && message.ends_with("?") {
-    "Calm down, I know what I'm doing!"
+      EMPTY
+  } else if is_shouting && is_question {
+      SHOUTING_QUESTION
   } else if is_shouting {
-    "Whoa, chill out!"
-  } else if message.ends_with("?") {
-    "Sure."
+      SHOUTING
+  } else if is_question {
+      QUESTION
   } else {
-    "Whatever."
+      RANDOM_MSG
   }
 }
