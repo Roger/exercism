3223d9039a1c75ed00a8b760bd2aaf37ff668974
roger <roger.duran@mobilityhouse.com>
2019-05-30 15:09:00 +0200

    add clock exercism

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+
+const MINUTES_PER_DAY: i32 = 60 * 24;
+
+fn euc_mod(x: i32, y: i32) -> i32 {
+    ((x % y) + y) % y
+}
+
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    pub minutes: i32,
+}
+
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let hours = euc_mod(self.minutes / 60, 24);
+        let minutes = self.minutes % 60;
+        write!(f, "{:02}:{:02}", hours, minutes)
+    }
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let minutes = euc_mod(hours * 60 + minutes, MINUTES_PER_DAY);
+
+        Clock { minutes }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(0, self.minutes + minutes)
+    }
+}
