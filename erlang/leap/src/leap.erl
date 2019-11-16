f90f3981b8cb818d3de69618a853142085f86b68
roger <rogerduran@gmail.com>
2019-11-16 01:44:50 +0100

    erlang/leap: simplify

--- erlang/leap/src/leap.erl
+++ erlang/leap/src/leap.erl
@@ -3,6 +3,7 @@
 -export([leap_year/1]).
 
 
-is_divisible(X, Y) -> X rem Y == 0.
-
-leap_year(Year) -> is_divisible(Year, 4) and not is_divisible(Year, 100) or is_divisible(Year, 400).
+leap_year(Year) when Year rem 400 =:= 0-> true;
+leap_year(Year) when Year rem 100 =:= 0-> false;
+leap_year(Year) when Year rem 4 =:= 0-> true;
+leap_year(_) -> false.
